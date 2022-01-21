9eb6b07301eabc2b8cc517dbdb775837dcc8b177
Yash <yashchudasama1722@gmail.com>
2022-01-21 16:51:56 +0530

    Added splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'startup_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const StartupScreen()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(10, 13, 44, 1),
+      body: Stack(children: [
+        Center(
+          child: Image.asset('assets/logo.png'),
+        ),
+        const Positioned(
+          bottom: 40,
+          left: 0,
+          right: 0,
+          child: Text(
+            "VideoSDK",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 26,
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
